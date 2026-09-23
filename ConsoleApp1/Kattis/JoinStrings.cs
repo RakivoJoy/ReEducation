4da0b3fa20f5bfb235da24bf6c7011d418acd7b7
Rak <nont>
2026-09-23 14:18:12 +0200

    Inital JoinStrings. FAILING due index out of bounds.

---
+++ ConsoleApp1/Kattis/JoinStrings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+class JoinStrings
+{
+    public static void Main()
+    {
+
+        TextReader tIn = Console.In;
+        TextWriter tOut = Console.Out;
+
+        string start = tIn.ReadLine();
+        string[] starts = start.Split(' ');
+        int X = int.Parse(starts[0]);
+
+        string[] lines = new string[X];
+
+        for(int i = 0; i < X; i++)
+        {
+            lines[i] = tIn.ReadLine();
+        }
+
+
+        int lastIndex = -1;
+        for (int i = 0; i < X-1; i++)
+        {
+            string operations = tIn.ReadLine();
+            string[] ops = start.Split(' ');
+            int a = int.Parse(ops[0]);
+            int b = int.Parse(ops[1]);
+
+            lines[a] = lines[a] + lines[b];
+            lines[b] = "";
+
+            lastIndex = a;
+        }
+
+        tOut.WriteLine(lines[lastIndex]);
+    }
+}
