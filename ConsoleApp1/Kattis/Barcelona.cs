e45c5a9a497a6856c7c95800b56c5e8fce990c53
Rak <nont>
2026-09-23 12:27:31 +0200

    First 8 Queens

--- ConsoleApp1/Kattis/Barcelona.cs
+++ ConsoleApp1/Kattis/Barcelona.cs
@@ -1,49 +1,43 @@
 ﻿using System;
 using System.IO;
 
-class InTest
+
+TextReader tIn = Console.In;
+TextWriter tOut = Console.Out;
+
+
+string start = tIn.ReadLine();
+
+string[] starts = start.Split(' ');
+
+int amount = int.Parse(starts[0]);
+int id = int.Parse(starts[1]);
+
+string bags = tIn.ReadLine();
+
+
+string[] bag = bags.Split(' ');
+
+int[] bagArray = new int[amount];
+
+for (int i = 0; i < amount; i++)
 {
-    public static void Main()
+    bagArray[i] = int.Parse(bag[i]);
+
+    if(bagArray[i] == id)
     {
-
-        TextReader tIn = Console.In;
-        TextWriter tOut = Console.Out;
-
-
-        string start = tIn.ReadLine();
-
-        string[] starts = start.Split(' ');
-
-        int amount = int.Parse(starts[0]);
-        int id = int.Parse(starts[1]);
-
-        string bags = tIn.ReadLine();
-
-
-        string[] bag = bags.Split(' ');
-
-        int[] bagArray = new int[amount];
-
-        for (int i = 0; i < amount; i++)
+        if(i == 0)
         {
-            bagArray[i] = int.Parse(bag[i]);
-
-            if(bagArray[i] == id)
-            {
-                if(i == 0)
-                {
-                    tOut.WriteLine("fyrst");
-                }
-                else if (i == 1)
-                {
-                    tOut.WriteLine("naestfyrst");
-                }
-                else
-                {
-                    tOut.WriteLine(i+1 + " fyrst");
-                }
-                return;
-            }
+            tOut.WriteLine("fyrst");
         }
+        else if (i == 1)
+        {
+            tOut.WriteLine("naestfyrst");
+        }
+        else
+        {
+            tOut.WriteLine(i+1 + " fyrst");
+        }
+        return;
     }
 }
---
+++ ConsoleApp1/Kattis/EightQueens.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+
+class EightQueens
+{
+
+    static char[,] board;
+
+    static TextReader tIn = Console.In;
+    static TextWriter tOut = Console.Out;
+
+    public static void Main()
+    {
+
+
+        board = new char[8,8];
+
+        // Read the board from input
+        for (int i = 0; i < 8; i++)
+        {
+            string s = tIn.ReadLine();
+            for (int j = 0; j < 8; j++)
+            {
+                board[i, j] = s[j];
+            }
+        }
+
+
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                char c = board[i, j];
+                if(c.Equals('*'))
+                {
+                    if(IsInLineOfQueen(i, j))
+                    {
+                        tOut.WriteLine("invalid");
+                        return; // Early exit if a queen can attack another
+                    }
+                }
+            }
+        }
+
+        tOut.WriteLine("valid");
+        return;
+    }
+
+    private static bool IsInLineOfQueen(int i, int j)
+    {
+        // Check row
+        for (int k = 0; k < 8; k++)
+        {
+            if (board[i, k] == '*' && k != j)
+            {
+                return true;
+            }
+        }
+
+        // Check column
+        for (int k = 0; k < 8; k++)
+        {
+            if (board[k, j] == '*' && k != i)
+            {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        for (int k = -7; k <= 7; k++)
+        {
+            if (i + k >= 0 && i + k < 8 && j + k >= 0 && j + k < 8)
+            {
+                if (board[i + k, j + k] == '*' && !(i + k == i && j + k == j))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
